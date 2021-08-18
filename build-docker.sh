5427ce5c07fd5b5a5ddf144ed20fff3a72092156
tcinbis <tcinbis@gmail.com>
2021-08-18 16:05:47 +0200

    Removing not used Dockerfile for building scion

--- build-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-mkdir docker-build-results
-docker build -t scion-build --build-arg USER_ID="$(id -u)" --build-arg GROUP_ID="$(id -g)" .
-./scion.sh bazel_remote
-docker run -it --rm --network bazel_remote_default -v "$(pwd)"/docker-build-results:/build-res scion-build /bin/bash -c "make flowtele && cp -R bazel-bin/* /build-res && exit"
